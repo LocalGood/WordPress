b76dd49e1077224cceb23e05bb302984d9c40917
soutaito <soutaito@gmail.com>
2017-10-12 14:41:28 +0900

    Adjust layout

--- themes/localgood/single-place.php
+++ themes/localgood/single-place.php
@@ -101,22 +101,19 @@ else :
 						</div>
 					</div>
 				<?php endif; ?>
-
-				<div class="common_single_gmap">
-					<h2 class="c-group_title01"><?php the_title(); ?>さんの場所</h2>
-					<div class="side_block gmap">
-						<?php
-						$data_lonlat = get_post_lonlat_attr();
-						if ( ! empty( $data_lonlat ) ) :
-							?>
+				<?php
+				$data_lonlat = get_post_lonlat_attr();
+				if ( ! empty( $data_lonlat ) ) :
+					?>
+					<div class="common_single_gmap">
+						<h2 class="c-group_title01"><?php the_title(); ?>さんの場所</h2>
+						<div class="side_block gmap">
 							<div id="gmap" <?php echo $data_lonlat; ?> style="height: 400px;margin-bottom:20px;"
 								 data-type="<?php echo esc_html( $post->post_type ); ?>"
 								 data-title="<?php the_title(); ?>"></div>
-							<?php
-						endif;
-						?>
+						</div>
 					</div>
-				</div>
+				<?php endif; ?>
 			<?php endif; ?>
 		<?php endif; ?>
 		<?php
